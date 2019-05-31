213cbc5e6c4effb83ba6fc6ffe3610c964343147
Akshay Chaudhari <30290337+AKSHAYCHAUDHARI1996@users.noreply.github.com>
2019-05-31 19:07:44 +0530

    Create 1.12 print input one word per line.c

---
+++ K & R programs/Chapter 1/1.12 print input one word per line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define IN 1
+#define OUT 0
+int main()
+{
+    int c,state; 
+    state = OUT;
+
+    while( (c=getchar()) != EOF ) {
+        if(c == ' ' || c == '\t' ||c =='\n') {
+            state = OUT;
+            putchar('\n');
+        } else if ( state == OUT ) {
+            state = IN;
+        }
+        
+        if(state == IN) {
+            putchar(c);
+        }
+    }
+return 0;
+}
